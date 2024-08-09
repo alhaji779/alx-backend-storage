48d8c07b807f390d06a54eafac8fbb9c97ad853c
Michael Kanu <michaelokanu01@yahoo.com>
2024-08-09 01:00:50 +0000

    create view

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,12 @@
+-- They score are under (strict) to 80
+-- AND no last_meeting date OR more than a month
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting AS
+    SELECT name
+        FROM students
+        WHERE score < 80 AND
+            (
+                last_meeting IS NULL
+                OR last_meeting < SUBDATE(CURRENT_DATE(), INTERVAL 1 MONTH)
+            )
+;
