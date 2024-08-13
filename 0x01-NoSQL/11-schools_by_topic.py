71840e19bfd57f1b9b868dff60d476842fcb95ba
Michael Kanu <michaelokanu01@yahoo.com>
2024-08-13 08:32:16 -0700

    11-school-by-topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" select data given condition """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ fetch data from collection """
+    n_list = [doc for doc in mongo_collection.find({"topic": topic})]
+    return n_list
