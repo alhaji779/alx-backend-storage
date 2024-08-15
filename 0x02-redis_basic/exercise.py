eac9e8bfdc6b5b064c08a7ce73c45ba33563c931
Michael Kanu <michaelokanu01@yahoo.com>
2024-08-15 21:59:37 +0000

    redis 4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,13 +18,30 @@ def count_calls(method: Callable) -> Callable:
     return invoker
 
 
+def call_history(method: Callable) -> Callable:
+    '''Tracks the call details of a method in a Cache class.
+    '''
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        '''Returns the method's output after storing its inputs and output.
+        '''
+        in_key = '{}:inputs'.format(method.__qualname__)
+        out_key = '{}:outputs'.format(method.__qualname__)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(in_key, str(args))
+        output = method(self, *args, **kwargs)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(out_key, output)
+        return output
+    return invoker
+
+
 class Cache:
     """ Class to store data in Redis """
     def __init__(self) -> None:
         """ init file """
         self._redis = redis.Redis()
         self._redis.flushdb(True)
-
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Store data in redis storage and return key
@@ -33,18 +50,15 @@ class Cache:
         self._redis.set(data_key, data)
         return data_key
 
-
     def get(self, key: str, fn: Callable = None,
             ) -> Union[str, bytes, int, float]:
         """ Retrive value from redis """
         data = self._redis.get(key)
         return fn(data) if fn is not None else data
 
-
     def get_str(self, key: str) -> str:
         """ retrieve a string """
         return self.get(key, lambda x: x.decode('utf-8'))
-
 
     def get_int(self, key: str) -> int:
         """ retrieves an interger from redis"""
